c51f37e1c420f7eb2e5791f552adcb8697e1a0d8
cuimengru <442402330@qq.com>
2020-06-02 10:34:38 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+
+        //来检查用户是否已登录
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
